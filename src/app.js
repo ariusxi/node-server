da6b7aec2106f50508bcf8d63c342769da0a7fe6
Alef Felix <alef.developerweb@gmail.com>
2018-08-08 10:43:36 -0300

    CRUD parte 1

--- src/app.js
+++ src/app.js
@@ -7,15 +7,21 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-//Criando rota
+//Criando rotas
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API",
-        version: "0.0.1"
+        version: "0.0.2"
     });
 });
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
 //Atribuindo rota
 app.use('/', route);
+app.use('/products', create);
 
 //Exportando APP
 module.exports = app;
