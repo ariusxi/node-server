859f1532d83ade8d5f9845c870f618f50f2bf8fa
Alef Felix <alef.developerweb@gmail.com>
2018-08-08 18:02:12 -0300

    Listando Produtos

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const controller = require('../controllers/product-controller');
 
 //definindo cada tipo de requisição
+router.get('/', controller.get);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
