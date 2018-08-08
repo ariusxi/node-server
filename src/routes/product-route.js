68c125552c84d3539e681e51de602f7d4555c5e7
Alef Felix <alef.developerweb@gmail.com>
2018-08-08 18:09:07 -0300

    Pegando produto pelo slug

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -7,6 +7,7 @@ const controller = require('../controllers/product-controller');
 
 //definindo cada tipo de requisição
 router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
