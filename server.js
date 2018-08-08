a04157d024945c3df921b0222b4135d33e2cb3b5
Alef Felix <alef.developerweb@gmail.com>
2018-08-08 10:04:55 -0300

    Primeiro Commit

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+//Importando modulos do servidor
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+//criando aplicação e definindo porta
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+//criando servidor
+const server = http.createServer(app);
+const router = express.Router();
+
+//configurando rotas
+var route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: 'Node Store API',
+        version: '0.0.1'
+    });
+});
+
+//definindo caminho das rotas
+app.use('/', route);
+
+server.listen(port);
+console.log('API rodando na porta ' + port);
