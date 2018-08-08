26c6c3f160b55fae348814715f985e60a879a0e3
Alef Felix <alef.developerweb@gmail.com>
2018-08-08 12:53:06 -0300

    CRUD parte 2

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@
 
 //Importando módulos
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //Criando aplicação
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Criando rotas
 const route = router.get('/', (req, res, next) => {
@@ -18,10 +22,23 @@ const route = router.get('/', (req, res, next) => {
 const create = router.post('/', (req, res, next) => {
     res.status(201).send(req.body);
 });
+const put = router.put('/:id', (req, res, next) => {
+    let id = req.params.id;
+    res.status(200).send({
+        id: id, 
+        item: req.body
+    });
+});
+const del = router.delete('/:id', (req, res, next) => {
+    let id = req.params.id;
+    res.status(200).send(req.body);
+});
 
 //Atribuindo rota
 app.use('/', route);
 app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 //Exportando APP
 module.exports = app;
