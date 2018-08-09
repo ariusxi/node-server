c61a3e76d983ae48ce5dc79a9872d84da3f6b37a
Alef Felix <alef.developerweb@gmail.com>
2018-08-09 09:35:12 -0300

    Pegando produtos pelo id

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -8,6 +8,7 @@ const controller = require('../controllers/product-controller');
 //definindo cada tipo de requisição
 router.get('/', controller.get);
 router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
