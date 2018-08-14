2824a1a480eaa20dbeeab02eb5b2619ed2e4ef78
Alef Felix <alef.developerweb@gmail.com>
2018-08-14 10:47:40 -0300

    Implementando validação de ações por token

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -3,6 +3,9 @@
 const repository = require('../repositories/order-repository');
 const guid = require('guid');
 
+//Importando service
+const authService = require('../services/auth-service');
+
 exports.get = async(req, res, next) => {
     try{
         var data = await repository.get();
@@ -16,8 +19,11 @@ exports.get = async(req, res, next) => {
 
 exports.post = async(req, res, next) => {
     try{
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+        const data = await authService.decodeToken(token);
+
         await repository.create({
-            customer: req.body.customer,
+            customer: data.id,
             number: guid.raw().substring(0, 6),
             items: req.body.items
         });
