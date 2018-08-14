b601be69b413cef0531e1fa0f79e4631e1c11216
Alef Felix <alef.developerweb@gmail.com>
2018-08-14 10:25:41 -0300

    Autenticação

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -4,13 +4,14 @@ const express = require('express');
 const router = express.Router();
 //chamando o controller
 const controller = require('../controllers/product-controller');
+const authService = require('../services/auth-service');
 
 //definindo cada tipo de requisição
 router.get('/', controller.get);
 router.get('/:slug', controller.getBySlug);
 router.get('/admin/:id', controller.getById);
 router.get('/tags/:tag', controller.getByTag);
-router.post('/', controller.post);
+router.post('/', authService.authorize, controller.post);
 router.put('/:id', controller.put);
 router.delete('/:id', controller.delete);
 
