ae7d9f22dcf1a912303bf94c8ebe16dcfda13074
Alef Felix <alef.developerweb@gmail.com>
2018-08-14 11:24:52 -0300

    Resolvendo Header Access

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 //Habilita o CORS
-app.use(() => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
